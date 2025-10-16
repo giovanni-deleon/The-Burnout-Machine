6f3a8d32f88ca0847b5ad7d961665f65903c95b5
Giovanni De Leon <giothermal1234@gmail.com>
2025-10-16 07:56:30 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,11 +3,12 @@
 
 import pandas as pd
 import matplotlib.pyplot as plt
+from CompareHoursWorked import compare_hours_worked
+from lLoadDataPandas import load_data_pandas
 
 # Global Variables
 df = None
 
-# METHOD DEFINITIONS
 # Main menu
 def main_menu():
     global df
@@ -25,25 +26,12 @@ def main_menu():
         main_menu()
 
     if user_choice == 2: # Supposed to display scatter plot comparing 2 student data points.
-        compare_hours_worked(df)
+        compare_hours_worked(df, 'Work_Hours', 'Burnout_Scale', "Hours Spent at Work", "Burnout Scale")
         user_choice = 0
         main_menu()
     #main_menu()
     if user_choice == 3: exit() # Option to end program.
 
-
-def load_data_pandas(): # Creates Pandas DataFrame
-    df = pd.read_csv("simulated_Student_burnOut_One.txt")
-    print(df.head(), ",")
-    return df
-
-
-
-def compare_hours_worked(df): # Supposed to display scatter plot comparing student data.
-    wh = df['Work_Hours'].tolist()
-    bs = df['Burnout_Scale'].tolist()
-    plt.scatter(wh, bs)
-    plt.show()
 
 # START OF PROGRAM
 main_menu()
